80b612959568a5a0298b06f92f3ce84e87cfd9f1
igasm <iga.smarz@gmail.com>
2018-12-30 12:45:31 +0100

    simple validation

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Link, { LinkedComponent } from 'valuelink';
 import { Input } from 'valuelink/tags';
+import './AddUser.css';
 
 class AddUser extends LinkedComponent {
     state = {
@@ -11,23 +12,42 @@ class AddUser extends LinkedComponent {
 
     onSubmit = e => { alert(JSON.stringify(this.state)) }
 
+    validateEmail = (email) => {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
     render(){
+
+        const nameLink = Link.state(this, 'name');
+        const nameIsValid = nameLink.value && nameLink.value.indexOf(' ')<0;
+
+        const emailLink = Link.state(this, 'email');
+        const emailIsValid = emailLink.value && this.validateEmail(emailLink.value);
+
         return (
-            <form onSubmit={this.onSubmit} >
+            <form className='form' onSubmit={this.onSubmit} >
 
-                <label> Name: 
-                    <Input valueLink={this.linkAt('name')} />
-                </label>
+                <div className={nameIsValid ? "validInput" : "invalidInput"}>
+                    <label> Name: 
+                        <Input valueLink={nameLink} />
+                    </label>
+                </div>
 
-                <label> Email: 
-                    <Input valueLink={this.linkAt('email')} />
-                </label>
+                <div className={emailIsValid ? "validInput" : "invalidInput"}>
+                    <label> Email: 
+                        <Input valueLink={emailLink} />
+                        <div>
+                            { emailIsValid ? '' : 'Email is required and must be valid'}
+                        </div>
+                    </label>
+                </div>
 
                 <label> Is active: 
-                    <Input checkedLink={this.linkAt('isActive')}/>
+                    <Input type="checkbox" checkedLink={Link.state(this,'isActive')}/>
                 </label>
 
-                <button type='submit'>Add user</button>
+                <button disabled={!nameIsValid || !emailIsValid} type='submit'>Add user</button>
 
             </form>
         );
