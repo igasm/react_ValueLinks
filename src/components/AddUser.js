cd5061b384d7826fbadb78c842a34c2f6761faf5
igasm <iga.smarz@gmail.com>
2018-12-30 11:20:13 +0100

    form uses valuelink lib

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,28 +1,8 @@
 import * as React from 'react';
+import Link, { LinkedComponent } from 'valuelink';
+import { Input } from 'valuelink/tags';
 
-function linkState( component, attr ){
-    return {
-        value: component.state[attr],
-        set(x){
-            component.setState({[attr]: x});
-        }
-    }
-}
-
-const Input = ({link, ...other}) => {
-    const {type, ...restProps} = other;
-    let input;
-
-    switch(type){
-        case 'checkbox': input = <input {...other} onChange= { e => link.set(e.target.checked) } checked={link.value}/>; break;
-        default: input = <input {...other} onChange= { e => link.set(e.target.value) } value={link.value}/>
-    }
-    
-    return input;
-}
-    
-
-class AddUser extends React.Component {
+class AddUser extends LinkedComponent {
     state = {
         name: '',
         email: '',
@@ -36,15 +16,15 @@ class AddUser extends React.Component {
             <form onSubmit={this.onSubmit} >
 
                 <label> Name: 
-                    <Input type='text' link={linkState(this, 'name')} />
+                    <Input valueLink={this.linkAt('name')} />
                 </label>
 
                 <label> Email: 
-                    <Input type='text' link={linkState(this, 'email')} />
+                    <Input valueLink={this.linkAt('email')} />
                 </label>
 
                 <label> Is active: 
-                    <Input type='checkbox' link={linkState(this, 'isActive')}/>
+                    <Input checkedLink={this.linkAt('isActive')}/>
                 </label>
 
                 <button type='submit'>Add user</button>
